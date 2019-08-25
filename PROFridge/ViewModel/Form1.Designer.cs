0a93087ff1bc2fdfb3a6b92f4e283180f20e08ba
Kjergaard <KjergaardVFX@gmail.com>
2019-08-26 01:12:49 +0200

    UI Update + Hotkey Start & Stop

--- PROFridge/ViewModel/Form1.Designer.cs
+++ PROFridge/ViewModel/Form1.Designer.cs
@@ -57,18 +57,19 @@
             this.label13 = new System.Windows.Forms.Label();
             this.txtbx_status = new System.Windows.Forms.TextBox();
             this.chkbx_UseFishRod = new System.Windows.Forms.CheckBox();
-            this.btn_botStart = new System.Windows.Forms.Button();
             this.txtbx_currentPP1 = new System.Windows.Forms.TextBox();
             this.chkbx_farmSpecifPoke = new System.Windows.Forms.CheckBox();
             this.txtbx_farmSpcifPoke = new System.Windows.Forms.TextBox();
             this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // txtbx_currentHealth
             // 
             this.txtbx_currentHealth.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "CurrentHealth", true));
-            this.txtbx_currentHealth.Location = new System.Drawing.Point(86, 60);
+            this.txtbx_currentHealth.Location = new System.Drawing.Point(92, 78);
             this.txtbx_currentHealth.Name = "txtbx_currentHealth";
             this.txtbx_currentHealth.ReadOnly = true;
             this.txtbx_currentHealth.Size = new System.Drawing.Size(23, 20);
@@ -77,7 +78,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 63);
+            this.label1.Location = new System.Drawing.Point(13, 81);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 1;
@@ -86,7 +87,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(401, 15);
+            this.label3.Location = new System.Drawing.Point(409, 97);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(66, 13);
             this.label3.TabIndex = 5;
@@ -95,7 +96,7 @@
             // txtbx_pokemonID
             // 
             this.txtbx_pokemonID.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "EncounterPokeIndex", true));
-            this.txtbx_pokemonID.Location = new System.Drawing.Point(473, 12);
+            this.txtbx_pokemonID.Location = new System.Drawing.Point(481, 94);
             this.txtbx_pokemonID.Name = "txtbx_pokemonID";
             this.txtbx_pokemonID.ReadOnly = true;
             this.txtbx_pokemonID.Size = new System.Drawing.Size(82, 20);
@@ -104,7 +105,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(409, 43);
+            this.label4.Location = new System.Drawing.Point(417, 125);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(58, 13);
             this.label4.TabIndex = 7;
@@ -113,7 +114,7 @@
             // txtbx_fightState
             // 
             this.txtbx_fightState.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "IsFight", true));
-            this.txtbx_fightState.Location = new System.Drawing.Point(473, 40);
+            this.txtbx_fightState.Location = new System.Drawing.Point(481, 122);
             this.txtbx_fightState.Name = "txtbx_fightState";
             this.txtbx_fightState.ReadOnly = true;
             this.txtbx_fightState.Size = new System.Drawing.Size(82, 20);
@@ -122,7 +123,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(252, 406);
+            this.label2.Location = new System.Drawing.Point(252, 374);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 13);
             this.label2.TabIndex = 9;
@@ -131,7 +132,7 @@
             // txtbx_xPos
             // 
             this.txtbx_xPos.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "XPos", true));
-            this.txtbx_xPos.Location = new System.Drawing.Point(243, 422);
+            this.txtbx_xPos.Location = new System.Drawing.Point(243, 390);
             this.txtbx_xPos.Name = "txtbx_xPos";
             this.txtbx_xPos.ReadOnly = true;
             this.txtbx_xPos.Size = new System.Drawing.Size(42, 20);
@@ -140,7 +141,7 @@
             // txtbx_yPos
             // 
             this.txtbx_yPos.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "YPos", true));
-            this.txtbx_yPos.Location = new System.Drawing.Point(305, 422);
+            this.txtbx_yPos.Location = new System.Drawing.Point(305, 390);
             this.txtbx_yPos.Name = "txtbx_yPos";
             this.txtbx_yPos.ReadOnly = true;
             this.txtbx_yPos.Size = new System.Drawing.Size(41, 20);
@@ -149,7 +150,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(229, 425);
+            this.label5.Location = new System.Drawing.Point(229, 393);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(14, 13);
             this.label5.TabIndex = 12;
@@ -158,7 +159,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(291, 425);
+            this.label6.Location = new System.Drawing.Point(291, 393);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(14, 13);
             this.label6.TabIndex = 13;
@@ -167,7 +168,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(8, 427);
+            this.label7.Location = new System.Drawing.Point(8, 395);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(41, 13);
             this.label7.TabIndex = 15;
@@ -176,7 +177,7 @@
             // txtbx_pokeDollar
             // 
             this.txtbx_pokeDollar.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "PokeDollars", true));
-            this.txtbx_pokeDollar.Location = new System.Drawing.Point(52, 424);
+            this.txtbx_pokeDollar.Location = new System.Drawing.Point(52, 392);
             this.txtbx_pokeDollar.Name = "txtbx_pokeDollar";
             this.txtbx_pokeDollar.ReadOnly = true;
             this.txtbx_pokeDollar.Size = new System.Drawing.Size(82, 20);
@@ -185,7 +186,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(7, 93);
+            this.label8.Location = new System.Drawing.Point(13, 111);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(73, 13);
             this.label8.TabIndex = 17;
@@ -194,7 +195,7 @@
             // txtbx_enemyCurrentHealth
             // 
             this.txtbx_enemyCurrentHealth.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "EnemyCurrentHealth", true));
-            this.txtbx_enemyCurrentHealth.Location = new System.Drawing.Point(85, 90);
+            this.txtbx_enemyCurrentHealth.Location = new System.Drawing.Point(91, 108);
             this.txtbx_enemyCurrentHealth.Name = "txtbx_enemyCurrentHealth";
             this.txtbx_enemyCurrentHealth.ReadOnly = true;
             this.txtbx_enemyCurrentHealth.Size = new System.Drawing.Size(24, 20);
@@ -203,37 +204,38 @@
             // lbl_abilityPP
             // 
             this.lbl_abilityPP.AutoSize = true;
-            this.lbl_abilityPP.Location = new System.Drawing.Point(11, 165);
+            this.lbl_abilityPP.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_abilityPP.Location = new System.Drawing.Point(19, 165);
             this.lbl_abilityPP.Name = "lbl_abilityPP";
-            this.lbl_abilityPP.Size = new System.Drawing.Size(51, 13);
+            this.lbl_abilityPP.Size = new System.Drawing.Size(61, 13);
             this.lbl_abilityPP.TabIndex = 18;
             this.lbl_abilityPP.Text = "Ability PP";
             // 
             // txtbx_abilityPP1
             // 
             this.txtbx_abilityPP1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "AbilityPP1", true));
-            this.txtbx_abilityPP1.Location = new System.Drawing.Point(43, 183);
+            this.txtbx_abilityPP1.Location = new System.Drawing.Point(51, 183);
             this.txtbx_abilityPP1.Name = "txtbx_abilityPP1";
             this.txtbx_abilityPP1.Size = new System.Drawing.Size(24, 20);
             this.txtbx_abilityPP1.TabIndex = 19;
             // 
             // txtbx_abilityPP2
             // 
-            this.txtbx_abilityPP2.Location = new System.Drawing.Point(43, 209);
+            this.txtbx_abilityPP2.Location = new System.Drawing.Point(51, 209);
             this.txtbx_abilityPP2.Name = "txtbx_abilityPP2";
             this.txtbx_abilityPP2.Size = new System.Drawing.Size(24, 20);
             this.txtbx_abilityPP2.TabIndex = 20;
             // 
             // txtbx_abilityPP3
             // 
-            this.txtbx_abilityPP3.Location = new System.Drawing.Point(43, 235);
+            this.txtbx_abilityPP3.Location = new System.Drawing.Point(51, 235);
             this.txtbx_abilityPP3.Name = "txtbx_abilityPP3";
             this.txtbx_abilityPP3.Size = new System.Drawing.Size(24, 20);
             this.txtbx_abilityPP3.TabIndex = 21;
             // 
             // txtbx_abilityPP4
             // 
-            this.txtbx_abilityPP4.Location = new System.Drawing.Point(42, 261);
+            this.txtbx_abilityPP4.Location = new System.Drawing.Point(50, 261);
             this.txtbx_abilityPP4.Name = "txtbx_abilityPP4";
             this.txtbx_abilityPP4.Size = new System.Drawing.Size(24, 20);
             this.txtbx_abilityPP4.TabIndex = 22;
@@ -241,7 +243,7 @@
             // label111
             // 
             this.label111.AutoSize = true;
-            this.label111.Location = new System.Drawing.Point(5, 186);
+            this.label111.Location = new System.Drawing.Point(13, 186);
             this.label111.Name = "label111";
             this.label111.Size = new System.Drawing.Size(34, 13);
             this.label111.TabIndex = 23;
@@ -250,7 +252,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(5, 212);
+            this.label9.Location = new System.Drawing.Point(13, 212);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(34, 13);
             this.label9.TabIndex = 24;
@@ -259,7 +261,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(5, 238);
+            this.label10.Location = new System.Drawing.Point(13, 238);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(34, 13);
             this.label10.TabIndex = 25;
@@ -268,7 +270,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(5, 261);
+            this.label11.Location = new System.Drawing.Point(13, 261);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(34, 13);
             this.label11.TabIndex = 26;
@@ -277,25 +279,27 @@
             // label12
             // 
             this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.Location = new System.Drawing.Point(8, 9);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(72, 13);
+            this.label12.Size = new System.Drawing.Size(181, 31);
             this.label12.TabIndex = 27;
             this.label12.Text = "FRIDGE BOT";
             // 
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(229, 6);
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(278, 9);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(68, 13);
+            this.label13.Size = new System.Drawing.Size(79, 13);
             this.label13.TabIndex = 29;
             this.label13.Text = "Game Status";
             // 
             // txtbx_status
             // 
             this.txtbx_status.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "Status", true));
-            this.txtbx_status.Location = new System.Drawing.Point(162, 22);
+            this.txtbx_status.Location = new System.Drawing.Point(211, 25);
             this.txtbx_status.Name = "txtbx_status";
             this.txtbx_status.ReadOnly = true;
             this.txtbx_status.Size = new System.Drawing.Size(202, 20);
@@ -304,28 +308,18 @@
             // chkbx_UseFishRod
             // 
             this.chkbx_UseFishRod.AutoSize = true;
-            this.chkbx_UseFishRod.Location = new System.Drawing.Point(8, 308);
+            this.chkbx_UseFishRod.Location = new System.Drawing.Point(447, 214);
             this.chkbx_UseFishRod.Name = "chkbx_UseFishRod";
             this.chkbx_UseFishRod.Size = new System.Drawing.Size(101, 17);
             this.chkbx_UseFishRod.TabIndex = 30;
             this.chkbx_UseFishRod.Text = "Use FishingRod";
             this.chkbx_UseFishRod.UseVisualStyleBackColor = true;
             // 
-            // btn_botStart
-            // 
-            this.btn_botStart.Location = new System.Drawing.Point(435, 393);
-            this.btn_botStart.Name = "btn_botStart";
-            this.btn_botStart.Size = new System.Drawing.Size(106, 38);
-            this.btn_botStart.TabIndex = 31;
-            this.btn_botStart.Text = "RUN BOT";
-            this.btn_botStart.UseVisualStyleBackColor = true;
-            this.btn_botStart.Click += new System.EventHandler(this.button1_Click);
-            // 
             // txtbx_currentPP1
             // 
             this.txtbx_currentPP1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtbx_currentPP1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "AbilityPP1", true));
-            this.txtbx_currentPP1.Location = new System.Drawing.Point(85, 183);
+            this.txtbx_currentPP1.Location = new System.Drawing.Point(93, 183);
             this.txtbx_currentPP1.Name = "txtbx_currentPP1";
             this.txtbx_currentPP1.ReadOnly = true;
             this.txtbx_currentPP1.Size = new System.Drawing.Size(24, 20);
@@ -334,7 +328,7 @@
             // chkbx_farmSpecifPoke
             // 
             this.chkbx_farmSpecifPoke.AutoSize = true;
-            this.chkbx_farmSpecifPoke.Location = new System.Drawing.Point(8, 341);
+            this.chkbx_farmSpecifPoke.Location = new System.Drawing.Point(447, 241);
             this.chkbx_farmSpecifPoke.Name = "chkbx_farmSpecifPoke";
             this.chkbx_farmSpecifPoke.Size = new System.Drawing.Size(118, 17);
             this.chkbx_farmSpecifPoke.TabIndex = 33;
@@ -344,7 +338,7 @@
             // txtbx_farmSpcifPoke
             // 
             this.txtbx_farmSpcifPoke.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "FarmSpecifPokeID", true));
-            this.txtbx_farmSpcifPoke.Location = new System.Drawing.Point(8, 364);
+            this.txtbx_farmSpcifPoke.Location = new System.Drawing.Point(447, 264);
             this.txtbx_farmSpcifPoke.Name = "txtbx_farmSpcifPoke";
             this.txtbx_farmSpcifPoke.Size = new System.Drawing.Size(118, 20);
             this.txtbx_farmSpcifPoke.TabIndex = 34;
@@ -354,15 +348,38 @@
             // 
             this.form1BindingSource.DataSource = typeof(PROFridge.Form1);
             // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label14.Location = new System.Drawing.Point(439, 374);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(100, 17);
+            this.label14.TabIndex = 35;
+            this.label14.Text = "F6 TO STOP";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label15.Location = new System.Drawing.Point(439, 349);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(109, 17);
+            this.label15.TabIndex = 36;
+            this.label15.Text = "F5 TO START";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(578, 450);
+            this.ClientSize = new System.Drawing.Size(583, 425);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
             this.Controls.Add(this.txtbx_farmSpcifPoke);
             this.Controls.Add(this.chkbx_farmSpecifPoke);
             this.Controls.Add(this.txtbx_currentPP1);
-            this.Controls.Add(this.btn_botStart);
             this.Controls.Add(this.chkbx_UseFishRod);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.txtbx_status);
@@ -431,10 +448,11 @@
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.TextBox txtbx_status;
         private System.Windows.Forms.CheckBox chkbx_UseFishRod;
-        private System.Windows.Forms.Button btn_botStart;
         private System.Windows.Forms.TextBox txtbx_currentPP1;
         private System.Windows.Forms.CheckBox chkbx_farmSpecifPoke;
         private System.Windows.Forms.TextBox txtbx_farmSpcifPoke;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
     }
 }
 
